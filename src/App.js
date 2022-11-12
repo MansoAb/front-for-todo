0c3513d2a9a63e40528b52cc8a26921ddf7c2246
Абубакаров Мансур Хайжахметович <abubakarovmansurhajzahmetovic@MacBook-Air-Abubakarov.local>
2022-11-13 01:49:26 +0300

    vela so har yin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  patchTodos,
+  postTodos,
+  deleteTodos,
+  fetchTodos,
+} from "./features/todosSlice";
 
 function App() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.loading);
+
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
+  const handleAd = (id, bl) => {
+    dispatch(patchTodos({ id, bl }));
+  };
+
+  const handleDel = (id) => {
+    dispatch(deleteTodos(id));
+  };
+
+  const handleText = (e) => {
+    setText(e.target.value);
+  };
+  const handlePost = (text) => {
+    dispatch(postTodos(text));
+    setText("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <input type="text" onChange={handleText} value={text} />
+        <input
+          type="button"
+          onClick={() => handlePost(text)}
+          value="добавить"
+        />
+      </div>
+
+      <div className="todos">
+        {loading ? (
+          todos.map((item, index) => {
+            return (
+              <div
+                key={item.id}
+                className={`todoBlock ${item.bl ? "added" : null}`}
+              >
+                <button onClick={() => handleAd(item._id, item.bl)}>✅</button>
+                <h1 className="textTodo">{item.text}</h1>
+                <button onClick={() => handleDel(item._id)}>❌</button>
+              </div>
+            );
+          })
+        ) : (
+          <span>Загрузка...</span>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/features/todosSlice.js
@@ -0,0 +1,111 @@
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [],
+  loading: false,
+};
+
+export const fetchTodos = createAsyncThunk(
+  "todos/fetch",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:3001/todos");
+      return await res.json();
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  }
+);
+
+export const deleteTodos = createAsyncThunk(
+  "todos/delete",
+  async (id, thunkAPI) => {
+    try {
+      await fetch(`http://localhost:3001/todos/${id}`, {
+        method: "DELETE",
+      });
+      return id;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  }
+);
+
+export const patchTodos = createAsyncThunk(
+  "todos/patch",
+  async (act, thunkAPI) => {
+    try {
+      console.log(act.id, act.bl);
+      const res = await fetch(`http://localhost:3001/todos/${act.id}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          bl: !act.bl,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      return act.id;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  }
+);
+
+export const postTodos = createAsyncThunk(
+  "todos/post",
+  async (text, thunkAPI) => {
+    try {
+      const res = await fetch(`http://localhost:3001/todos`, {
+        method: "POST",
+        body: JSON.stringify({
+          bl: false,
+          text: text,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+      return text;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
+    }
+  }
+);
+
+export const ad = createAction("ad");
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTodos.fulfilled, (state, action) => {
+        state.todos = action.payload;
+        state.loading = true;
+      })
+      .addCase(fetchTodos.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(patchTodos.fulfilled, (state, action) => {
+        state.todos = state.todos.map((item) => {
+          if (item._id === action.payload) {
+            item.bl = !item.bl;
+          }
+          return item;
+        });
+      })
+      .addCase(deleteTodos.fulfilled, (state, action) => {
+        state.todos = state.todos.filter((item) => {
+          return item._id !== action.payload;
+        });
+      })
+      .addCase(postTodos.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.todos.unshift({ text: action.payload, bl: false });
+      });
+  },
+});
+
+export default todosSlice.reducer;
